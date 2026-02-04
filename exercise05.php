76aa9b0bbc109f81c7703aed0a9b7d5837812f7f
Ayeshan Kavindu <ayeshankavindu10@gmail.com>
2026-02-04 16:54:36 +0100

    5th

--- exercise05.php
+++ exercise05.php
@@ -6,3 +6,29 @@
  * Add static methods add(), subtract(), and multiply().
  * Use these methods to perform mathematical operations on two numbers.
  */
+<?php
+class Math
+{
+    public static function add(float $number1, float $number2):float
+    {
+        return $number1 + $number2;
+    }
+    public static function subtract(float $number1, float $number2):float
+    {
+        return $number1 - $number2;
+    }
+    public static function multiply(float $number1, float $number2):float
+    {
+        return $number1 * $number2;
+    }
+
+}
+echo "Addition: " . Math::add(10, 5) . "<br>";
+echo "Subtraction: " . Math::subtract(10, 5) . "<br>";
+echo "Multiplication: " . Math::multiply(10, 5) . "<br>";
+
+
+
+
+
+?>
