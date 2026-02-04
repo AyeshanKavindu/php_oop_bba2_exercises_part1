6d19433d38a0dff8d0eb810d90e4d487895dea43
Ayeshan Kavindu <ayeshankavindu10@gmail.com>
2026-02-04 16:12:19 +0100

    1stdone

--- exercise01.php
+++ exercise01.php
@@ -8,3 +8,42 @@
  * Add the getters and setters for each property.
  * Below the class, instantiate a Game and display its information.
 */
+class Game {
+    public function __construct(
+     public string $name,
+     public string $editor,
+     public float $price,
+     )
+    {
+    }
+     public function getName(): string
+    {
+        return $this->name;
+    }
+     public function getEditor(): string
+    {
+        return $this->editor;
+    }
+     public function getPrice(): float
+    {
+        return $this->price;
+    }
+     public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+     public function setEditor(string $editor): void
+    {
+        $this->editor = $editor;
+    }
+     public function setPrice(float $price): void
+    {
+        $this->price = $price;
+}
+     
+}
+$game = new Game("Call of duty", "Activision", 59.99);     
+
+echo "Game Name: " . $game->getName() . "\n";
+echo "Editor: " . $game->getEditor() . "\n";
+echo "Price: $" . $game->getPrice() . "\n";    
