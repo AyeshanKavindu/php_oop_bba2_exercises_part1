6061ef88b3eed3513175fd63d65a83de1ebe52a7
Ayeshan Kavindu <ayeshankavindu10@gmail.com>
2026-02-04 16:28:58 +0100

    3rd

--- exercise03.php
+++ exercise03.php
@@ -8,3 +8,43 @@
  * Override the constructor and call the parent constructor.
  * Override the displayInfo() method to display the salary and position.
  */
+
+require_once 'exercise02.php';
+
+class Employee extends Person
+{
+    public function __construct(
+        string $firstName,
+        string $lastName,
+        protected int $age,
+        protected float $salary,
+        protected string $position
+    ) {
+        parent::__construct($firstName, $lastName, $age);
+    }
+
+    public function getSalary(): float
+    {
+        return $this->salary;
+    }
+
+    public function setSalary(float $salary): void
+    {
+        $this->salary = $salary;
+    }
+
+    public function getPosition(): string
+    {
+        return $this->position;
+    }
+
+    public function setPosition(string $position): void
+    {
+        $this->position = $position;
+    }
+
+    public function displayInfo(): string
+    {
+        return parent::displayInfo() . ", <br> Salary: $" . number_format($this->salary, 2) . ", Position: " . $this->position;
+    }
+}
